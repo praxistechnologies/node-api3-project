bbcf7a1fe99c17baf5dc2b637144d8cd587f7ffb
Praxis Technologies <ptanalytics@gmail.com>
2021-08-06 18:03:38 -0400

    code push

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,78 @@
+const Users = require('../users/users-model');
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const date = new Date();
+  console.log(`
+  METHOD: ${req.method}, 
+  URL: ${req.baseUrl}, 
+  TIMESTAMP: ${date.toLocaleString()}`)
+  next()
 }
+
+// ${new Date(Date.now())}
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  const { id } = req.params;
+  Users.getById(id)
+  .then(user => {
+    if(user) {
+      req.user = user
+      next()
+    } else {
+      next({
+        status: 404,
+        message: "user not found"
+      })
+    }
+  })
+  .catch(next)
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({
+      message: "missing required name field"
+    })
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  const { text } = req.body
+  if (!text) {
+    res.status(400).json({
+      message: "missing required text field"
+    })
+  } else {
+    next()
+  }
+}
+
+function notFound(req, res, next) {
+  res.status(404).json({
+    message: 'not found, sorry!'
+  })
+}
+
+function errorHandling(err, req, res, next) {
+  const status = err.status || 500
+  res.status(status).json({
+    message: err.message
+  })
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+  notFound,
+  errorHandling
+}
--- api/server.js
+++ api/server.js
@@ -1,3 +1,5 @@
+  
+const path = require('path')
 const express = require('express');
 
 const server = express();
@@ -5,9 +7,20 @@ const server = express();
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
+server.use(express.json());
+server.use(express.static(path.join(__dirname, 'api/')))
+
+const usersRouter = require('./users/users-router');
+const { logger, notFound, errorHandling } = require('./middleware/middleware');
+
+server.use('/api/users', logger, usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+server.use('*', notFound);
+
+server.use(errorHandling);
 
 module.exports = server;
